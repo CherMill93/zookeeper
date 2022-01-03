4294f624144f1b691667cf666c1aa9ec41e61543
CherMill <cmille2016@gmail.com>
2022-01-02 18:45:52 -0700

    11.1.6 complete

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express') 
+const PORT = process.env.PORT || 3001; //allows the port # to automatically set to the best option | Heroku requires 80 and this will allow it to be set to 80 rather than 3001 exclusively
 const app = express(); //starter code to use express | We assign express() to the app variable so that we can later chain on methods to the Express.js server.
 
 function filterByQuery(query, animalsArray) {
@@ -48,8 +49,8 @@ app.get('/api/animals', (req, res) => { // get() method requires two arguments. 
   res.json(results); //we are using the send() method from the res parameter (short for response) to send the string Hello! to our client. - this was changed in the next step of module |
 });
 
-app.listen(3001, () => {
-  console.log(`API server now on port 3001!`); //the port is to classroom/building like a web address is to the copllege campus
+app.listen(PORT, () => {
+  console.log(`API server now on port ${PORT}!`); //the port is to classroom/building like a web address is to the copllege campus
 });
 
 const {animals} = require('./data/animals.json'); //path in ''
